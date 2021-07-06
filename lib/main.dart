7d49317f09bb0421cecec2567e97f130992c26d6
Nkayoum <fares.nkayoum@2tcorp.org>
2021-07-06 19:50:27 +0200

    remplace de nnnnnnnnnnnnnnnnnnn

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Test git application',
+      title: 'nnnnnnnnnnnnnnnnnnnnnnnnnnnnnn',
       theme: ThemeData(
         // This is the theme of your application.
         //
