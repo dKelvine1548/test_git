fec7e3f5e4435b7ebe280dc0f5da8c238b015969
dkelvine1548 <71010229+dKelvine1548@users.noreply.github.com>
2021-07-06 17:09:53 +0100

    modification du titre du home page

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Page d acceuil'),
+      home: MyHomePage(title: 'Page d acceuill'),
     );
   }
 }
