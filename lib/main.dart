1e8b6b57cfa5b06eb86cff78129a5035ad16ef9a
dkelvine1548 <71010229+dKelvine1548@users.noreply.github.com>
2021-07-06 18:47:47 +0100

    ajout de "s"

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Test git application',
+      title: 'Test git applications',
       theme: ThemeData(
         // This is the theme of your application.
         //
